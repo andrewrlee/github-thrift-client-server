3ed8e10f8661249e9743d182e81232c65c3e64d2
plasma147 <plasma147@hotmail.com>
2015-03-01 17:55:41 +0000

    Updating run-service.sh

--- web-interface/release/run-service.sh
+++ web-interface/release/run-service.sh
@@ -2,5 +2,5 @@
 
 exec 2>&1
 exec > /var/log/web-interface.log
-exec java -jar /opt/web-interface/web-interface-0.1.0-SNAPSHOT-standalone.jar
+exec java -jar /opt/web-interface/web-interface-0.0.1-SNAPSHOT.jar
 
